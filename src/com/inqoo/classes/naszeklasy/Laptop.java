1ea9c357e9ff83295f355babe99b04450c85cec8
j.slabik@interia.pl <INQ77#ooProg>
2020-07-12 11:37:28 +0200

    Laptop Jacka

--- src/com/inqoo/classes/naszeklasy/Laptop.java
+++ src/com/inqoo/classes/naszeklasy/Laptop.java
@@ -1,16 +1,19 @@
 package com.inqoo.classes.naszeklasy;
 
 public class Laptop {
-    private String brand; //marka
+    public String brand; //marka
     private int rdzenie; //liczba rzedni w procesorze
     public boolean bluetooth; //czy posiada bluetooth
-    double RAM; //ilość RAM
+    public double RAM; //ilość RAM
+
+    public Laptop() {}
 
     public Laptop(String brand, int rdzenie, boolean bluetooth, double RAM) {
         this.brand = brand;
         this.rdzenie = rdzenie;
         this.bluetooth = bluetooth;
         this.RAM = RAM;
+
     }
 }
 
