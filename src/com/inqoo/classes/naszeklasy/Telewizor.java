20323649ea0418b73192aeba085c45f04d8fc3ce
annabarbaraniemiec <52847607+annabarbaraniemiec@users.noreply.github.com>
2020-07-12 11:45:05 +0200

    dodano Telewizor (#6)
    * dodano Telewizor
    
    * poprawki Telewizor, dodano MainTelewizor

---
+++ src/com/inqoo/classes/naszeklasy/Telewizor.java
@@ -0,0 +1,30 @@
+package com.inqoo.classes.naszeklasy;
+
+public class Telewizor {
+
+    public String marka;
+    public String kolor;
+    public int rozdzielczosc;
+
+    public Telewizor(String marka, String kolor, int rozdzielczosc) {
+        this.marka = marka;
+        this.kolor = kolor;
+        this.rozdzielczosc = rozdzielczosc;
+    }
+
+    public static void main(String[] args) {
+
+        Telewizor panasonic = new Telewizor("Panasonic", "szary", 40);
+        Telewizor lg = new Telewizor("LG", "czarny", 52);
+
+        System.out.println(panasonic);
+        System.out.println(lg);
+    }
+}
+
+
+
+
+
+
+
