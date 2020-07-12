3608e0a0c855cec9803508fed8dd85ed075a0ef7
magdalenagoralczyk <magdalena.goralczyk@poczta.onet.pl>
2020-07-12 11:11:29 +0200

    do poprawienia

---
+++ src/com/inqoo/classes/naszeklasy/Klasa1a.java
@@ -0,0 +1,33 @@
+package com.inqoo.classes.naszeklasy;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Klasa1a {
+
+
+    private String szkoła;
+    private String klasaa;
+    private int rocznik;
+    private double uczniowie;
+    String ulubionycolor;
+    private boolean biolchem;
+
+    public Car(String szkoła, String klasaa, int rocznik, double ucznowie, String ulubionycolor, boolean biolchem);
+    this.szkoła = szkoła;
+    this.klasaa = klasaa;
+    this. rocznik = rocznik;
+    this.uczniowie = uczniowie;
+    this.ulubionycolor = ulubionycolor;
+    this.biolchem = biolchem;
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
+
+
+
+}
