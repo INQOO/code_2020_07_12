49cb16b8bcdc5ccd2b26e9ec8e755edfc11c64ca
magdalenagoralczyk <magdalena.goralczyk@poczta.onet.pl>
2020-07-12 11:48:40 +0200

    coś dodane

--- src/com/inqoo/classes/naszeklasy/Klasa1a.java
+++ src/com/inqoo/classes/naszeklasy/Klasa1a.java
@@ -1,32 +1,32 @@
 package com.inqoo.classes.naszeklasy;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class Klasa1a {
-
 
     private String szkola;
     private String klasaa;
     private int rocznik;
     private double uczniowie;
-    String ulubionycolor;
+    private String ulubionyColor;
     private boolean biolchem;
 
-    public Car(String szkola, String klasaa, int rocznik, double ucznowie, String ulubionycolor, boolean biolchem);
-    this.szkola = szkola;
-    this.klasaa = klasaa;
-    this. rocznik = rocznik;
-    this.uczniowie = uczniowie;
-    this.ulubionycolor = ulubionycolor;
-    this.biolchem = biolchem;
-
-    public static void main(String[] args) {
+    public Klasa1a(String szkola, String klasaa, int rocznik, double uczniowie, String ulubionyColor, boolean biolchem) {
+        this.szkola = szkola;
+        this.klasaa = klasaa;
+        this.rocznik = rocznik;
+        this.uczniowie = uczniowie;
+        this.ulubionyColor = ulubionyColor;
+        this.biolchem = biolchem;
 
     }
 
+        public String getSzkola() { return szkola; }
+        public String getKlasaa() { return klasaa; }
+        public int getRocznik() { return rocznik; }
+        public double getUczniowie() { return uczniowie; }
+        public String getUlubionyColor() {return ulubionyColor; }
+        public boolean getBiolchem() {return biolchem; }
 
 
-}
 
 
 
