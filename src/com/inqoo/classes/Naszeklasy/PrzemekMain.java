c0f650e3d4e91c83bbc65079c467aea376bcbe9c
tavirek <67555259+tavirek@users.noreply.github.com>
2020-07-12 11:42:59 +0200

    Dodano Telefony (#8)
    * Dodano Telefony
    
    * Poprawiono Telefony

---
+++ src/com/inqoo/classes/Naszeklasy/PrzemekMain.java
@@ -0,0 +1,13 @@
+package com.inqoo.classes.Naszeklasy;
+
+public class PrzemekMain {
+    public static void main(String[] args) {
+        Telefony moj = new Telefony(
+                "Honor",
+                "viev20",
+                5.8,
+                "niebieski",
+                true);
+        System.out.println(moj.brand);
+    }
+}
---
+++ src/com/inqoo/classes/Naszeklasy/Telefony.java
@@ -0,0 +1,20 @@
+package com.inqoo.classes.Naszeklasy;
+
+public class Telefony {
+    public String brand;
+    public String model;
+    public double wielkoscEkranu;
+    public String color;
+    public boolean umnieDziala;
+
+    public Telefony(String brand, String model, double wielkoscEkranu, String color, boolean umnieDziala) {
+        this.brand = brand;
+        this.model = model;
+        this.wielkoscEkranu = wielkoscEkranu;
+        this.color = color;
+        this.umnieDziala = umnieDziala;
+    }
+
+}
+
+
