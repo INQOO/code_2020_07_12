f44e0e8c16cc79fe848af2f28a1354c17d601ff1
LukaszHajdukIT <onevisage@interia.pl>
2020-07-12 11:45:51 +0200

    Add properties

--- src/com/inqoo/classes/naszeklasy/Monitor.java
+++ src/com/inqoo/classes/naszeklasy/Monitor.java
@@ -7,13 +7,18 @@ public class Monitor {
     private int przekatna;
     private String wyswietlacz;
 
-        public Monitor(String firma, String rozdzielczosc, int przekatna, String wyswietlacz )
+        public Monitor(String firma, String rozdzielczosc, int przekatna, String wyswietlacz)
         {
            this.firma = firma;
            this.rozdzielczosc = rozdzielczosc;
            this.przekatna = przekatna;
            this.wyswietlacz = wyswietlacz;
         }
+
+    public String getFirma() { return firma; }
+    public String getRozdzielczosc() { return rozdzielczosc; }
+    public int getPrzekatna() { return przekatna; }
+    public String getWyswietlacz() { return wyswietlacz; }
 
     }
 
