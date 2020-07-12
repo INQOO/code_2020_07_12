06af1922819319a0ccc09528d1cb89ccc3b54f72
Lenovo <patryk_st@wp.pl>
2020-07-12 11:08:44 +0200

    Add Smart

---
+++ src/com/inqoo/classes/naszeklasy/Smart.java
@@ -0,0 +1,32 @@
+package com.inqoo.classes.naszeklasy;
+
+public class Smart {
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+
+    public Smart (String brand, String model, int year, String color){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+
+    }
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() { return color; }
+
+
+
+}
