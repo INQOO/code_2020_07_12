1839a29429861d2507fd6171deddc5246a0161b4
Renovatio89 <lachpawel89@gmail.com>
2020-07-12 11:27:42 +0200

    Change name variable

--- src/com/inqoo/classes/naszeklasy/Komputer.java
+++ src/com/inqoo/classes/naszeklasy/Komputer.java
@@ -5,14 +5,14 @@ public class Komputer {
     private String procesor;
     private int pamiec;
     public String grafika;
-    private int dysk;
+    private int pojemnoscDysku;
     private boolean windows;
 
     public Komputer(String procesor, int pamiec, String grafika, int dysk, boolean windows) {
         this.procesor = procesor;
         this.pamiec = pamiec;
         this.grafika = grafika;
-        this.dysk = dysk;
+        this.pojemnoscDysku = dysk;
         this.windows = windows;
 
 
