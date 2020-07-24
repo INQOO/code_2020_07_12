8ff7d8886c25b4804da52b611017f163969e0d43
j.slabik@interia.pl <INQ77#ooProg>
2020-07-24 16:41:17 +0200

    Laptop Jacka

--- src/com/inqoo/homework/jacek/Employee.java
+++ src/com/inqoo/homework/jacek/Employee.java
@@ -2,44 +2,46 @@ package com.inqoo.homework.jacek;
 
 import java.math.BigDecimal;
 
-public class Employee {
-    public String name;
-    public int age;
-    public String position;
-    public BigDecimal salary = new BigDecimal("3000");
-    public static final double bonus = 1.2;
+class Employee {
+    private String name;
+    private int age;
+    private String position;
+    private BigDecimal salary = new BigDecimal("3000");
+    private static final double BONUS = 1.2;
 
-    public Employee(String name, int age, String position){
+    Employee(String name, int age, String position){
         this.name = name;
         this.age = age;
         this.position = position;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public int getAge() {
+    int getAge() {
+
         return age;
     }
 
-    public String getPosition() {
+    String getPosition() {
         return position;
     }
 
-    public void increaseSalary(BigDecimal amount){
+    void increaseSalary(BigDecimal amount){
         salary = salary.add(amount);
     }
 
-    public void decreaseSalary(BigDecimal amount){
+    void decreaseSalary(BigDecimal amount){
         salary = salary.subtract(amount);
     }
 
-    public void addBonus() {
-        salary = new BigDecimal(bonus).multiply(salary).setScale(2, BigDecimal.ROUND_HALF_DOWN);
+    void addBonus() {
+
+        salary = new BigDecimal(BONUS).multiply(salary).setScale(2, BigDecimal.ROUND_HALF_DOWN);
     }
 
-    public BigDecimal getSalary(){
+    BigDecimal getSalary(){
         return salary;
     }
 
--- src/com/inqoo/homework/jacek/MainEmployer.java
+++ src/com/inqoo/homework/jacek/MainEmployer.java
@@ -1,7 +1,5 @@
 package com.inqoo.homework.jacek;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.math.BigDecimal;
 
 public class MainEmployer {
@@ -12,12 +10,12 @@ public class MainEmployer {
         Employee employee3 = new Employee("Olaf", 29, "Supervisor");
 
         employee1.increaseSalary(new BigDecimal("500"));
-        System.out.println("Wynagrodzenie pracownika " + employee1.getName() + " wynosi: " + employee1.getSalary());
+        System.out.println("Wynagrodzenie pracownika " + employee1.getName() + ", lat: " + employee1.getAge() + "," + " wynosi: " + employee1.getSalary());
 
         employee2.decreaseSalary(new BigDecimal("300"));
         employee2.addBonus();
-        System.out.println("Wynagrodzenie pracownika " + employee2.getName() + " wynosi: " + employee2.getSalary());
+        System.out.println("Wynagrodzenie pracownika " + employee2.getName() + " piastującego stanowisko: " + employee2.getPosition() + "," + " wynosi: " + employee2.getSalary());
 
-        System.out.println(employee3.toString());
+      System.out.println(employee3);
     }
 }
